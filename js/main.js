db81ce30ba00b42380f46888cbfbe90161f53cb5
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-01 23:38:05 +0000

    increments the nextEntryId property of the data model so that if another entry is submitted later, it will receive a different entryId.

--- js/main.js
+++ js/main.js
@@ -5,4 +5,28 @@ $photoUrl.addEventListener('input', function (event) {
   $image.src = event.target.value;
 });
 
-// const $submit = document.querySelector('form');
+const $submitForm = document.querySelector('form');
+
+$submitForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const $titleValue = $submitForm.elements['title-text'].value;
+  const $photoUrlValue = $submitForm.elements['photo-url'].value;
+  const $notesValue = $submitForm.elements.notes.value;
+
+  const formData = {
+    title: $titleValue,
+    photoURL: $photoUrlValue,
+    notes: $notesValue,
+    entryID: data.nextEntryId,
+  };
+
+  data.nextEntryId++;
+
+  data.entries.unshift(formData);
+});
+
+// other way to write function code block
+// $submit.addEventListener('submit', handleSubmit);
+// function handleSubmit(event) {
+
+// }

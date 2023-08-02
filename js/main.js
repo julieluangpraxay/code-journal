a223fc410fb34925fae723b37b1586e4920a5357
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-02 22:11:36 +0000

    Create an event listener on the document which listens for the DOMContentLoaded event. In the callback function for that event, loop through the data.entries array and generate a DOM tree for each entry in the array, and append that DOM tree to the unordered list.  Remove the example HTML entries from index.html once the user's entries are successfully being appended to the DOM on page load.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// issue 1 code
 const $photoUrl = document.querySelector('#photo-url');
 const $image = document.querySelector('img');
 
@@ -24,4 +25,42 @@ $submitForm.addEventListener('submit', function (event) {
   data.entries.unshift(formData);
   $image.src = './images/placeholder-image-square.jpg';
   $submitForm.reset();
+});
+
+// issue 2 code
+
+function renderEntry(entry) {
+  // generate and return a DOM tree for a single entry that matches the entries created in the unordered list
+  const $entryList = document.createElement('li');
+  $entryList.className = 'row';
+
+  const $entryDiv = document.createElement('div');
+  $entryDiv.className = 'column-half';
+
+  const $entryImg = document.createElement('img');
+  $entryImg.setAttribute('src', entry.photoURL);
+
+  const $otherDiv = document.createElement('div');
+  $otherDiv.className = 'column-half';
+
+  const $h1Entry = document.createElement('h1');
+  $h1Entry.className = 'h1Entry';
+
+  const $pEntry = document.createElement('p');
+  $pEntry.className = 'pEntry';
+
+  $entryList.appendChild($entryDiv);
+  $entryDiv.appendChild($entryImg);
+  $otherDiv.appendChild($h1Entry);
+  $otherDiv.appendChild($entryList);
+
+  return $entryList;
+}
+
+const $ul = document.querySelector('.entry-list');
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const $data = renderEntry(data.entries[i]);
+    $ul.appendChild($data);
+  }
 });

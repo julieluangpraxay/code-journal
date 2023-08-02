2276b9c612a0fff33905c137636b06b4f2ccbc75
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-02 01:51:24 +0000

    Completed issue-1 and need to test the app now

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+const localStorageData = localStorage.getItem('data');
+window.addEventListener('beforeunload', function () {
+  const jsonData = JSON.stringify(data);
+  localStorage.setItem('data', jsonData);
+});
+
+if (localStorageData) {
+  data = JSON.parse(localStorageData);
+}
--- js/main.js
+++ js/main.js
@@ -21,8 +21,9 @@ $submitForm.addEventListener('submit', function (event) {
   };
 
   data.nextEntryId++;
-
   data.entries.unshift(formData);
+  $image.src = './images/placeholder-image-square.jpg';
+  $submitForm.reset();
 });
 
 // other way to write function code block
